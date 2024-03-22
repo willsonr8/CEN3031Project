eec796d030b9ce29a08af8e64b983d842cc6523f
willsonr8 <willsonr8@gmail.com>
2024-03-22 11:27:40 -0400

    Synced Django backend to Next frontend. Some structural mods.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello_view
+from .views import hello_view, get_data
+
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
+    path('api/data/', get_data, name='get_data'),
 ]
