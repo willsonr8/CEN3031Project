cbce1951c4a71982a5a19f2cc90d2cc174fad6fc
AnielWhittker <whittkermelian.a@ufl.edu>
2024-04-12 12:53:45 -0400

    The account page search functionality is working as intended.

--- next-frontend/src/app/AccountSettingsPage/page.tsx
+++ next-frontend/src/app/AccountSettingsPage/page.tsx
@@ -3,12 +3,25 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import NavBar from '../components/AccountNavbar';
 import DeleteButton from '../components/DeleteButton';
-import CheckTokenExpiration from '../components/TokenExpirationCheck';
 
 
 const SettingsPage: React.FC = () => {
 
-    CheckTokenExpiration();
+    const isTokenExpired = (token: string) => {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const exp = payload.exp * 1000;
+        const now = Date.now();
+        return now >= exp;
+    };
+    const router = useRouter();
+
+    useEffect(() => {
+        const accessToken = document.cookie.split('; ').find(row => row.startsWith('access='));
+        if (!accessToken || isTokenExpired(accessToken.split('=')[1])) {
+            router.push('/login');
+        }
+    }, [router]);
+
     return (
         <div style={{ textAlign: "center", fontSize: "24px" }}>
             <NavBar />
