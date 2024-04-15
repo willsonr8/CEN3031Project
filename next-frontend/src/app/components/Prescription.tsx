0457a866f9e8d1a462113ecae83e4af2d4719761
AnielWhittker <whittkermelian.a@ufl.edu>
2024-04-15 16:13:43 -0400

    fixed format

--- next-frontend/src/app/components/Prescription.tsx
+++ next-frontend/src/app/components/Prescription.tsx
@@ -33,7 +33,7 @@ const Prescriptions = () => {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-indexed
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${month}/${day}/${year}`;
   };
 
   useEffect(() => {
