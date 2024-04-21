074048205ab6b22ed6ddecae11341b112684a693
willsonr8 <willsonr8@gmail.com>
2024-04-21 16:10:13 -0400

    Added comments to DrugStoresMap

--- backend/api/RxNorm.py
+++ backend/api/RxNorm.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 # RxNorm API class
 class Rx:
--- next-frontend/src/app/components/DrugStoresMap.jsx
+++ next-frontend/src/app/components/DrugStoresMap.jsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 import Head from 'next/head';
 
 const DrugStoresMap =({ googleApiKey })=> {
+    // establish necessary variables and arrays
     let map, service, infoWindow;
     let userDraggedMap = false;
     let userZoomed = false;
     let markers = [];
 
+    // respond to unavailable geolocation
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(
@@ -17,11 +19,13 @@ const DrugStoresMap =({ googleApiKey })=> {
         infoWindow.open(map);
     }
 
+    // helper function to clear map markers where needed
     function clearMarkers(markers) {
         markers.forEach(marker => marker.setMap(null));
         markers = [];
     }
 
+    // map initialization
     async function initMap() {
         if (typeof google === 'undefined') {
             console.error('Google Maps API not loaded');
@@ -29,8 +33,8 @@ const DrugStoresMap =({ googleApiKey })=> {
         }
         //@ts-ignore
         const { Map } = await google.maps.importLibrary("maps");
-        const {AdvancedMarkerElement, PinElement} = await google.maps.importLibrary("marker");
 
+        // set startup view of map on center of USA
         map = new Map(document.getElementById("map"), {
             zoom: 4,
             center: {lat: 39.8283, lng: -98.5795},
@@ -40,6 +44,7 @@ const DrugStoresMap =({ googleApiKey })=> {
         service = new google.maps.places.PlacesService(map);
         infoWindow = new google.maps.InfoWindow();
 
+        // performs search of drug stores in map location and sets markers where needed
         function performSearch(userLocationMarker = null) {
             const request = {
                 query: "Drug Store",
@@ -47,16 +52,17 @@ const DrugStoresMap =({ googleApiKey })=> {
                 bounds: map.getBounds(),
             };
 
+            // executes drug store search on map
             service.textSearch(request, (results, status) => {
                 if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-                    // Clear existing markers
+                    // clear existing markers
                     clearMarkers(markers)
 
-                    // Fit bounds to display all markers
+                    // fit bounds to display all markers
                     const bounds = new google.maps.LatLngBounds();
+                    // add marker for each search result
                     results.forEach((place) => {
-                        // for unknown reasons this has to be marker. google.maps.marker.advancedmarkerelemnt does not work properly.
-                        const newInfoWindow = new google.maps.InfoWindow();
+                        // despite deprecation this has to be marker. AdvancedMarkerElement does not work properly.
                         const marker = new google.maps.Marker({
                             map: map,
                             position: place.geometry.location,
@@ -67,6 +73,7 @@ const DrugStoresMap =({ googleApiKey })=> {
                         } else {
                             bounds.union(place.geometry.location);
                         }
+                        // setup info window for each marker containing name and address on click event
                         google.maps.event.addListener(marker, "click", () => {
                             const content = document.createElement("div");
                             const nameElement = document.createElement("h2");
@@ -82,6 +89,7 @@ const DrugStoresMap =({ googleApiKey })=> {
                             infoWindow.open(map, marker);
                         });
                     });
+                    // limit re-searches to occur after 500 ms timeout
                     setTimeout(() => {
                         const bounds = new google.maps.LatLngBounds();
                         markers.forEach((marker) => bounds.extend(marker.position));
@@ -89,13 +97,15 @@ const DrugStoresMap =({ googleApiKey })=> {
                 }
             });
         }
+        // listen for zoom
         map.addListener("zoom_changed", () => {
             userZoomed = true;
         })
-
+        // listen for drag
         map.addListener("dragstart", () => {
             userDraggedMap = true;
         });
+        // listen for idle, if idle after drag or zoom then re-search
         map.addListener("idle", () => {
             if (userDraggedMap) {
                 performSearch();
@@ -110,12 +120,12 @@ const DrugStoresMap =({ googleApiKey })=> {
         let userLocationMarker = null;
 
         const locationButton = document.createElement("button");
-
+        // setup location search button
         locationButton.textContent = "Stores Near Me";
         locationButton.classList.add("custom-map-control-button");
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
         locationButton.addEventListener("click", () => {
-            // Try HTML5 geolocation.
+            // try HTML5 geolocation upon button click
             if (navigator.geolocation) {
                 clearMarkers(markers)
                 navigator.geolocation.getCurrentPosition(
@@ -124,7 +134,7 @@ const DrugStoresMap =({ googleApiKey })=> {
                             lat: position.coords.latitude,
                             lng: position.coords.longitude,
                         };
-
+                        // establish information window for current location
                         infoWindow.setPosition(pos);
                         infoWindow.setContent("Current Location");
                         infoWindow.open(map);
@@ -133,6 +143,7 @@ const DrugStoresMap =({ googleApiKey })=> {
                         if (userLocationMarker !== null) {
                             userLocationMarker.setMap(null);
                         }
+                        // place current location marker if location is given
                         userLocationMarker = new google.maps.Marker({
                             map: map,
                             position: pos,
@@ -150,6 +161,7 @@ const DrugStoresMap =({ googleApiKey })=> {
     }
 
     useEffect(() => {
+        // establish connection to google maps api using hidden api key
         const script = document.createElement('script');
         script.src = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&libraries=places&callback=initMap`;
         script.async = true;
@@ -162,6 +174,7 @@ const DrugStoresMap =({ googleApiKey })=> {
         };
     }, [googleApiKey]);
     return (
+        // style map and associated elements
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
             <Head>
                 <title>Geolocation</title>
